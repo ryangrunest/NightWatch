e6477c9953c401ce9d0aaf140ee14961c7040fe9
Ryan Grunest <ryangrunest@geeko>
2021-09-27 16:39:42 -0700

    initial commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Ryan Grunest on 9/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
