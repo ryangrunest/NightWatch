15eb06ab72e5cf437994f3a8683ee634b1f7f837
Ryan Grunest <ryangrunest@geeko>
2021-09-29 16:05:01 -0700

    break tasks into separate task lists

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -8,10 +8,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
       VStack {
-        Text("Nightly Tasks")
-        Text("Weekly Tasks")
-        Text("Monthly Tasks")
+        HStack {
+          VStack(alignment: .leading) {
+            Text("Nightly Tasks")
+              .font(.headline)
+              .foregroundColor(.gray)
+            Text("Check all windows")
+            Text("Check all doors")
+            Text("Check that the safe is locked")
+            Text("Check the mailbox")
+            Text("Inspect security cameras")
+            Text("Clear ice from sidewalks")
+            Text("Document \"strange and unusual\" occurences")
+            Spacer()
+          }
+          VStack(alignment: .leading) {
+            Text("Weekly Tasks")
+              .font(.headline)
+              .foregroundColor(.gray)
+            Spacer()
+          }
+          VStack(alignment: .leading) {
+            Text("Monthly Tasks")
+              .font(.headline)
+              .foregroundColor(.gray)
+            Spacer()
+          }
+        }.padding(.all, 12.0)
+        Spacer()
       }
+      
     }
 }
 
