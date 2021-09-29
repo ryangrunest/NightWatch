88d4dcdec42ef3a0eefd6442c2c4847192294888
Ryan Grunest <ryangrunest@geeko>
2021-09-29 15:15:32 -0700

    add multiple text fields

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ContentView.swift
 //  NightWatch
 //
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      Text("Nightly Tasks")
+      Text("Weekly Tasks")
+      Text("Monthly Tasks")
     }
 }
 
